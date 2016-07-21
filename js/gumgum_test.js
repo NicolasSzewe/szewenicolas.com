40c2086d383ecf6ae4d5ad74eee487ab358e2a16
szewenicolas <szewenicolas@users.noreply.github.com>
2016-07-20 20:58:27 -0700

    Test js

---
+++ js/gumgum_test.js
@@ -0,0 +1,11 @@
+var client_id = 1234567
+var safetyUrl = "http://10.16.236.188:8000/safety?u=" + document.location + "&cid=" + client_id
+
+var safetyJson = $.ajax({
+  	type: 'GET',
+  	url: safetyUrl,
+  	async:false
+	});
+
+console.log(safetyJson.responseText);
+//safetyJson = JSON.parse(safetyJson.responseText);
