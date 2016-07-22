ebb047f9fafe2926093e04ec76364ce7cd4e28ea
szewenicolas <szewenicolas@users.noreply.github.com>
2016-07-21 23:25:04 -0700

    add tag id

--- js/gumgum_test.js
+++ js/gumgum_test.js
@@ -1,5 +1,6 @@
 var client_id = 1
-var safetyUrl = "http://54.90.218.71:8000/safety?u=" + document.location + "&cid=" + client_id
+var tag_id = 58694023
+var safetyUrl = "http://54.90.218.71:8000/safety?u=" + document.location + "&cid=" + client_id + "&tid=" + tag_id
 
 var safetyJson = $.ajax({
   	type: 'GET',
