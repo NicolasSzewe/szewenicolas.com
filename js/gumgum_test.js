ddc6f550cb841f228d2d544c3cc7a1f9c07276b6
szewenicolas <szewenicolas@users.noreply.github.com>
2016-07-22 01:04:12 -0700

    add visitor id

--- js/gumgum_test.js
+++ js/gumgum_test.js
@@ -4,10 +4,11 @@ var country_code = "US"
 var region = "CA"
 var city = "Pasadena"
 var browser_type = 1
+var visitor_id = 86752431
 var page_url = encodeURIComponent(document.location)
 
 var safetyUrl = "http://54.90.218.71:8000/safety?u=" + page_url + "&cid=" + client_id + "&tid=" + tag_id 
-+ "&cc=" + country_code + "&r=" + region + "&c=" + city + "&b=" + browser_type
++ "&cc=" + country_code + "&r=" + region + "&c=" + city + "&b=" + browser_type + "&vid=" + visitor_id
 
 var safetyJson = $.ajax({
   	type: 'GET',
