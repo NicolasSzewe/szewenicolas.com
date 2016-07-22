4e454d841f6495ed5d89c23ba4d410ddc1647a4f
szewenicolas <szewenicolas@users.noreply.github.com>
2016-07-22 01:01:46 -0700

    add parameter and encode url

--- js/gumgum_test.js
+++ js/gumgum_test.js
@@ -1,6 +1,13 @@
 var client_id = 1
 var tag_id = 58694023
-var safetyUrl = "http://54.90.218.71:8000/safety?u=" + document.location + "&cid=" + client_id + "&tid=" + tag_id
+var country_code = "US"
+var region = "CA"
+var city = "Pasadena"
+var browser_type = 1
+var page_url = encodeURIComponent(document.location)
+
+var safetyUrl = "http://54.90.218.71:8000/safety?u=" + page_url + "&cid=" + client_id + "&tid=" + tag_id 
++ "cc=" + country_code + "&r=" + region + "&c=" + city + "&b=" + browser_type
 
 var safetyJson = $.ajax({
   	type: 'GET',
@@ -10,3 +17,4 @@ var safetyJson = $.ajax({
 
 console.log(safetyJson);
 //safetyJson = JSON.parse(safetyJson.responseText);
+
