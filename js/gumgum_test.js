d5f7f040a4fbe9ae7ec9884667cd06623917ea8d
szewenicolas <szewenicolas@users.noreply.github.com>
2016-07-22 01:02:27 -0700

    typo

--- js/gumgum_test.js
+++ js/gumgum_test.js
@@ -7,7 +7,7 @@ var browser_type = 1
 var page_url = encodeURIComponent(document.location)
 
 var safetyUrl = "http://54.90.218.71:8000/safety?u=" + page_url + "&cid=" + client_id + "&tid=" + tag_id 
-+ "cc=" + country_code + "&r=" + region + "&c=" + city + "&b=" + browser_type
++ "&cc=" + country_code + "&r=" + region + "&c=" + city + "&b=" + browser_type
 
 var safetyJson = $.ajax({
   	type: 'GET',
